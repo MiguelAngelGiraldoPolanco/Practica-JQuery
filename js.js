cac8370bce1c90909a6ac9d9d2097693d90e9348
MiguelAngel <miguelgiraldo1116@gmail.com>
2024-11-12 11:40:05 -0500

    Html y css Basicos

---
+++ js.js
@@ -0,0 +1,21 @@
+// Objetivo: Desarrollar una plataforma web interactiva que permita gestionar proyectos y tareas. Esta práctica se centrará en implementar funcionalidades avanzadas de jQuery para manipular el DOM y responder a eventos, incluyendo la edición en línea de los proyectos.
+
+// Descripción de la Actividad: Crear una aplicación web que permita al usuario:
+
+// Agregar y eliminar proyectos.
+// Agregar y eliminar tareas dentro de los proyectos.
+// Editar los nombres de los proyectos haciendo doble clic en ellos.
+// Resaltar tareas al pasar el ratón sobre ellas.
+// Marcar tareas como importantes y completadas.
+// Requisitos:
+
+// Agregar Proyectos y Tareas:
+// Un formulario de entrada que permita al usuario añadir proyectos y tareas.
+// Eliminar Proyectos y Tareas:
+// Botones de eliminación para eliminar proyectos y tareas.
+// Edición de Proyectos:
+// Los nombres de los proyectos deben ser editables mediante un doble clic, convirtiendo el texto en un campo de entrada.
+// Los cambios deben guardarse al presionar Enter o hacer clic fuera del campo de entrada.
+// Interactividad Visual:
+// Las tareas deben cambiar de estilo al pasar el ratón sobre ellas.
+// Se deben poder marcar tareas como "importantes" o "completadas", aplicando diferentes clases CSS.
